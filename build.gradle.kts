46c072359d2d954aacfc567566c6572a64d3c60f
Sam Snyder <sam@moderne.io>
2023-08-31 14:14:41 -0700

    Remove redundant dependency declarations

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,6 @@ description = "Rewrite recipes."
 // The bom version can also be set to a specific version or latest.release.
 val latest = "latest.integration"
 dependencies {
-    compileOnly("org.projectlombok:lombok:latest.release")
-    compileOnly("com.google.code.findbugs:jsr305:latest.release")
-    annotationProcessor("org.projectlombok:lombok:latest.release")
     implementation(platform("org.openrewrite:rewrite-bom:${latest}"))
 
     implementation("org.openrewrite:rewrite-java")
@@ -19,13 +16,7 @@ dependencies {
     // Need to have a slf4j binding to see any output enabled from the parser.
     runtimeOnly("ch.qos.logback:logback-classic:1.2.+")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:latest.release")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:latest.release")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:latest.release")
     testRuntimeOnly("com.google.guava:guava:latest.release")
-
-    testImplementation("org.openrewrite:rewrite-test")
-    testImplementation("org.assertj:assertj-core:latest.release")
 }
 
 configure<PublishingExtension> {
