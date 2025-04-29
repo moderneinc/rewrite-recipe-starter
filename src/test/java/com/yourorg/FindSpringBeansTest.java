23bc9c257225f59e64a6100286fb4b75d9f2e070
Tim te Beek <tim@moderne.io>
2025-04-29 11:45:20 +0200

    Apply code suggestions from bot

--- src/test/java/com/yourorg/FindSpringBeansTest.java
+++ src/test/java/com/yourorg/FindSpringBeansTest.java
@@ -2,6 +2,7 @@ package com.yourorg;
 
 import com.yourorg.table.SpringBeans;
 import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
@@ -23,6 +24,7 @@ class FindSpringBeansTest implements RewriteTest {
             .classpath("spring-context"));
     }
 
+    @DocumentExample
     @Test
     void findSpringBeans() {
         String filePath = "src/main/java/com/yourorg/MyConfig.java";
