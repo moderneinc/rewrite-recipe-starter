02015e8d234cc822a52767b1e789b37cacb4f1c0
Bryan Friedman <bryanfriedman@users.noreply.github.com>
2025-07-14 18:14:10 -0700

    Update UseIntegerValueOf.java

--- src/main/java/com/yourorg/UseIntegerValueOf.java
+++ src/main/java/com/yourorg/UseIntegerValueOf.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.yourorg;
 
 import org.openrewrite.ExecutionContext;
@@ -6,45 +21,55 @@ import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
 import org.openrewrite.java.JavaTemplate;
 import org.openrewrite.java.JavaVisitor;
-import org.openrewrite.java.search.UsesType;
+import org.openrewrite.java.MethodMatcher;
+import org.openrewrite.java.search.UsesMethod;
 import org.openrewrite.java.tree.Expression;
 import org.openrewrite.java.tree.J;
-import org.openrewrite.java.tree.TypeUtils;
+import org.openrewrite.java.tree.JavaType;
 
 public class UseIntegerValueOf extends Recipe {
 
-    public static final String INTEGER = "java.lang.Integer";
+    private static final MethodMatcher INTEGER_CONSTRUCTOR = new MethodMatcher("java.lang.Integer#<init>(*)");
 
     @Override
     public String getDisplayName() {
-        return "Use Integer.valueOf(x) instead of new Integer(x)";
+        return "Use Integer.valueOf(x) or Integer.parseInt(x) instead of new Integer(x)";
     }
 
     @Override
     public String getDescription() {
-        return "Replaces unnecessary boxing constructor calls with the more efficient Integer.valueOf(x).";
+        return "Replaces unnecessary boxing constructor calls with the more efficient Integer.valueOf(x) for int values, or Integer.parseInt(x) for String values.";
     }
 
     @Override
     public TreeVisitor<?, ExecutionContext> getVisitor() {
         return Preconditions.check(
-                new UsesType<>(INTEGER, false),
+                new UsesMethod<>(INTEGER_CONSTRUCTOR),
                 new JavaVisitor<ExecutionContext>() {
                     @Override
                     public J visitNewClass(J.NewClass newClass, ExecutionContext ctx) {
                         J update = super.visitNewClass(newClass, ctx);
-                        if (update instanceof J.NewClass) {
-                            J.NewClass nc = (J.NewClass) update;
-                            if (TypeUtils.isOfClassType(nc.getType(), INTEGER)) {
-                                Expression arg = nc.getArguments().get(0);
-                                return JavaTemplate.builder("Integer.valueOf(#{any()})")
-                                        .contextSensitive()
+                        if (!(update instanceof J.NewClass)) {
+                            return update;
+                        }
+                        J.NewClass nc = (J.NewClass) update;
+                        if (!INTEGER_CONSTRUCTOR.matches(nc)) {
+                            return nc;
+                        }
+
+                        Expression arg = nc.getArguments().get(0);
+                        if (arg.getType() instanceof JavaType.Class) {
+                            JavaType.Class clazz = (JavaType.Class) arg.getType();
+                            if ("java.lang.String".equals(clazz.getFullyQualifiedName())) {
+                                return JavaTemplate.builder("Integer.parseInt(#{any(java.lang.String)})")
                                         .build()
                                         .apply(getCursor(), nc.getCoordinates().replace(), arg);
                             }
-                            return nc;
                         }
-                        return update;
+
+                        return JavaTemplate.builder("Integer.valueOf(#{any()})")
+                                .build()
+                                .apply(getCursor(), nc.getCoordinates().replace(), arg);
                     }
                 }
         );
