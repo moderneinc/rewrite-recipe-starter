31f8fe23e9880dcc4b646b0da93e02c98cd15e2d
Bryan Friedman <bryanfriedman@users.noreply.github.com>
2025-07-22 16:14:17 -0700

    Proposed code solutions for advanced workshop labs

---
+++ src/main/java/com/yourorg/TrackJavaTodos.java
@@ -0,0 +1,119 @@
+package com.yourorg;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.jspecify.annotations.Nullable;
+import org.openrewrite.*;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.tree.*;
+import org.openrewrite.text.PlainText;
+import org.openrewrite.text.PlainTextParser;
+import org.openrewrite.text.PlainTextVisitor;
+
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Value
+@EqualsAndHashCode(callSuper = false)
+public class TrackJavaTodos extends ScanningRecipe<TrackJavaTodos.TodoComments> {
+
+    @Override
+    public String getDisplayName() {
+        return "Track TODOs from Java comments";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Scans Java source comments for TODOs and collects them.";
+    }
+
+    @Option(displayName = "Header",
+            description = "Header for TODO.md.",
+            example = "## To Do List",
+            required = false)
+    @Nullable
+    String header;
+
+    public static class TodoComments {
+        boolean foundTodoFile;
+        LinkedHashSet<String> todos = new LinkedHashSet<>();
+    }
+
+    @Override
+    public TodoComments getInitialValue(ExecutionContext ctx) {
+        return new TodoComments();
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getScanner(TodoComments acc) {
+        JavaIsoVisitor<ExecutionContext> javaIsoVisitor = new JavaIsoVisitor<ExecutionContext>() {
+            @Override
+            public Space visitSpace(Space space, Space.Location loc, ExecutionContext ctx) {
+                for (Comment comment : space.getComments()) {
+                    // Let's just match TextComments and ignore Javadoc comments
+                    if (comment instanceof TextComment) {
+                        String c = ((TextComment) comment).getText();
+                        if (c.contains("TODO")) {
+                            acc.todos.add(c);
+                        }
+                    }
+                }
+                return space;
+            }
+        };
+
+        return new TreeVisitor<Tree, ExecutionContext>() {
+            @Override
+            public @Nullable Tree visit(@Nullable Tree tree, ExecutionContext ctx) {
+                if (!(tree instanceof SourceFile)) {
+                    return tree;
+                }
+                SourceFile s = (SourceFile) tree;
+                if ("TODO.md".equals(s.getSourcePath().toString())) {
+                    acc.foundTodoFile = true;
+                }
+                if (javaIsoVisitor.isAcceptable(s, ctx)) {
+                    javaIsoVisitor.visit(tree, ctx);
+                }
+                return tree;
+            }
+        };
+    }
+
+    @Override
+    public Collection<? extends SourceFile> generate(TodoComments acc, ExecutionContext ctx) {
+        if (acc.foundTodoFile) {
+            return Collections.emptyList();
+        }
+        // If the file was not found, create it
+        return PlainTextParser.builder().build()
+                // We start with an empty string that we then append to in the visitor
+                .parse("")
+                // Be sure to set the source path for any generated file, so that the visitor can find it
+                .map(it -> (SourceFile) it.withSourcePath(Paths.get("TODO.md")))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor(TodoComments acc) {
+        return new PlainTextVisitor<ExecutionContext>() {
+            @Override
+            public PlainText visitText(PlainText text, ExecutionContext ctx) {
+                PlainText t = super.visitText(text, ctx);
+                // If the file is not TODO.md, don't modify it
+                if (!"TODO.md".equals(t.getSourcePath().toString())) {
+                    return t;
+                }
+                StringBuilder sb = new StringBuilder();
+                for (String todo : acc.todos) {
+                    sb.append(todo.trim()).append("\n");
+                }
+
+                String allComments = sb.toString();
+                String headerText = header == null ? "## To Do List" : header;
+                return t.withText((headerText + "\n" + allComments).trim());
+            }
+        };
+    }
+}
---
+++ src/main/java/com/yourorg/TrackTodos.java
@@ -0,0 +1,156 @@
+package com.yourorg;
+
+import com.yourorg.table.TodoCommentsReport;
+import com.yourorg.trait.TodoComment;
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.jspecify.annotations.Nullable;
+import org.openrewrite.*;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.tree.*;
+import org.openrewrite.text.PlainText;
+import org.openrewrite.text.PlainTextParser;
+import org.openrewrite.text.PlainTextVisitor;
+import org.openrewrite.xml.XmlIsoVisitor;
+import org.openrewrite.xml.tree.Xml;
+import org.openrewrite.yaml.YamlIsoVisitor;
+import org.openrewrite.yaml.tree.Yaml;
+
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Value
+@EqualsAndHashCode(callSuper = false)
+public class TrackTodos extends ScanningRecipe<TrackTodos.TodoComments> {
+
+    transient TodoCommentsReport todoCommentsTable = new TodoCommentsReport(this);
+
+    @Override
+    public String getDisplayName() {
+        return "Track TODOs from Java, YAML, or XML comments";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Scans Java, YAML, and XML source comments for TODOs and collects them.";
+    }
+
+    @Option(displayName = "Header",
+            description = "Header for TODO.md.",
+            example = "## To Do List",
+            required = false)
+    @Nullable
+    String header;
+
+    public static class TodoComments {
+        boolean foundTodoFile;
+        LinkedHashSet<TodoComment> todos = new LinkedHashSet<>();
+    }
+
+    @Override
+    public TodoComments getInitialValue(ExecutionContext ctx) {
+        return new TodoComments();
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getScanner(TodoComments acc) {
+        JavaIsoVisitor<ExecutionContext> javaIsoVisitor = new JavaIsoVisitor<ExecutionContext>() {
+            @Override
+            public @Nullable J visit(@Nullable Tree tree, ExecutionContext ctx) {
+                new TodoComment.Matcher().get(getCursor())
+                        .ifPresent(todos -> {
+                            acc.todos.add(todos);
+                        });
+                return super.visit(tree, ctx);
+            }
+        };
+
+        YamlIsoVisitor<ExecutionContext> yamlIsoVisitor = new YamlIsoVisitor<ExecutionContext>() {
+            @Override
+            public @Nullable Yaml visit(@Nullable Tree tree, ExecutionContext ctx) {
+                new TodoComment.Matcher().get(getCursor())
+                        .ifPresent(todos -> acc.todos.add(todos));
+                return super.visit(tree, ctx);
+            }
+        };
+
+        XmlIsoVisitor<ExecutionContext> xmlIsoVisitor = new XmlIsoVisitor<ExecutionContext>() {
+            @Override
+            public @Nullable Xml visit(@Nullable Tree tree, ExecutionContext ctx) {
+                new TodoComment.Matcher().get(getCursor())
+                        .ifPresent(todos -> acc.todos.add(todos));
+                return super.visit(tree, ctx);
+            }
+        };
+
+        return new TreeVisitor<Tree, ExecutionContext>() {
+            @Override
+            public @Nullable Tree visit(@Nullable Tree tree, ExecutionContext ctx) {
+                if (!(tree instanceof SourceFile)) {
+                    return tree;
+                }
+                SourceFile s = (SourceFile) tree;
+                if ("TODO.md".equals(s.getSourcePath().toString())) {
+                    acc.foundTodoFile = true;
+                }
+                if (javaIsoVisitor.isAcceptable(s, ctx)) {
+                    javaIsoVisitor.visit(tree, ctx);
+                }
+                if (yamlIsoVisitor.isAcceptable(s, ctx)) {
+                    yamlIsoVisitor.visit(tree, ctx);
+                }
+                if (xmlIsoVisitor.isAcceptable(s, ctx)) {
+                    xmlIsoVisitor.visit(tree, ctx);
+                }
+
+                return tree;
+            }
+        };
+    }
+
+    @Override
+    public Collection<? extends SourceFile> generate(TodoComments acc, ExecutionContext ctx) {
+        if (acc.foundTodoFile) {
+            return Collections.emptyList();
+        }
+        // If the file was not found, create it
+        return PlainTextParser.builder().build()
+                // We start with an empty string that we then append to in the visitor
+                .parse("")
+                // Be sure to set the source path for any generated file, so that the visitor can find it
+                .map(it -> (SourceFile) it.withSourcePath(Paths.get("TODO.md")))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor(TodoComments acc) {
+        return new PlainTextVisitor<ExecutionContext>() {
+            @Override
+            public PlainText visitText(PlainText text, ExecutionContext ctx) {
+                PlainText t = super.visitText(text, ctx);
+                // If the file is not TODO.md, don't modify it
+                if (!"TODO.md".equals(t.getSourcePath().toString())) {
+                    return t;
+                }
+                StringBuilder sb = new StringBuilder();
+                for (TodoComment todo : acc.todos) {
+                    for (String item : todo.getTodos()) {
+                        sb.append(item).append("\n");
+                        String sourcePath = todo.getCursor().firstEnclosingOrThrow(SourceFile.class).getSourcePath().toString();
+                        System.out.println(sourcePath);
+                        System.out.println(item);
+                        System.out.println(todo.getTree().getClass().toString());
+                        System.out.println();
+                        todoCommentsTable.insertRow(ctx, new TodoCommentsReport.Row(sourcePath, item, todo.getTree().getClass().toString()));
+                    }
+                }
+
+                String allComments = sb.toString();
+                String headerText = header == null ? "## To Do List" : header;
+                // Append the comments to the end of the file
+                return t.withText((headerText + "\n" + allComments).trim());
+            }
+        };
+    }
+}
