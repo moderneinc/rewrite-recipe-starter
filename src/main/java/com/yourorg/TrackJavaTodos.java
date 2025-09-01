e07483422d6e8e8184dd6dc1a0a30cc6d978d778
Tim te Beek <tim@moderne.io>
2025-09-01 12:05:25 +0200

    Minor polish

--- src/main/java/com/yourorg/TrackJavaTodos.java
+++ src/main/java/com/yourorg/TrackJavaTodos.java
@@ -1,10 +1,28 @@
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.yourorg;
 
 import com.yourorg.table.TodoCommentsReport;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
 import org.jspecify.annotations.Nullable;
-import org.openrewrite.*;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Recipe;
+import org.openrewrite.SourceFile;
+import org.openrewrite.TreeVisitor;
 import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.tree.Comment;
 import org.openrewrite.java.tree.Space;
@@ -18,12 +36,12 @@ public class TrackJavaTodos extends Recipe {
 
     @Override
     public String getDisplayName() {
-        return "Track TODOs from Java comments";
+        return "Export TODOs from Java comments";
     }
 
     @Override
     public String getDescription() {
-        return "Scans Java source comments for TODOs and collects them.";
+        return "Export TODOs from Java source comments into a data table.";
     }
 
     @Override
