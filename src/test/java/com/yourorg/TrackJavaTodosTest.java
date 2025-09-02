f650a0b96dc6e609d75ef591da0c2923d1aa9197
Bryan Friedman <bryanfriedman@users.noreply.github.com>
2025-09-02 10:48:13 -0700

    Reorder advanced workshop (#98)
    * Reorder advanced workshop
    
    Changes to main branch that correspond to https://github.com/moderneinc/rewrite-recipe-starter/pull/97
    
    * Apply formatter
    
    * Add missing newline at end of file
    
    * Update src/main/java/com/yourorg/TrackJavaTodos.java
    
    Co-authored-by: Tim te Beek <tim@moderne.io>
    
    * Update src/main/java/com/yourorg/TrackJavaTodos.java
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Tim te Beek <tim@moderne.io>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/test/java/com/yourorg/TrackJavaTodosTest.java
+++ src/test/java/com/yourorg/TrackJavaTodosTest.java
@@ -1,120 +1,64 @@
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.yourorg;
 
+import com.yourorg.table.TodoCommentsReport;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
-import java.nio.file.Path;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.openrewrite.java.Assertions.java;
-import static org.openrewrite.test.SourceSpecs.text;
 
 class TrackJavaTodosTest implements RewriteTest {
     @Override
     public void defaults(RecipeSpec spec) {
-        spec.recipe(new TrackJavaTodos("## Test Header"));
+        spec.recipe(new TrackJavaTodos());
     }
 
     @Disabled
     @DocumentExample
     @Test
-    void createNewTodoFile() {
+    void findTodos() {
         // Notice how the before text is doesNotExist(), indicating that the file does not exist yet.
         // The after text is the content of the file after the recipe is applied.
         rewriteRun(
-                //language=java
-            java(
+          spec -> spec.dataTable(TodoCommentsReport.Row.class, rows -> {
+              assertThat(rows).containsExactly(
+                new TodoCommentsReport.Row("A.java", "TODO: Have fun", "class org.openrewrite.java.tree.J$MethodDeclaration"),
+                new TodoCommentsReport.Row("A.java", "TODO: Test your code", "class org.openrewrite.java.tree.J$MethodDeclaration"),
+                new TodoCommentsReport.Row("A.java", "TODO: Learn", "class org.openrewrite.java.tree.J$Return"));
+          }),
+          //language=java
+          java(
             """
-          class A {
-              // TODO: Have fun
-              /* TODO: Test your code */
-              // Just a regular comment
-              public String foo() {
-                // TODO: Learn
-                return "bar";
-              }
-              // Another regular comment
-          }
-          """
-            ),
-            //language=markdown
-            text(
-                    doesNotExist(),
-                    """
-                      ## Test Header
-                      TODO: Have fun
-                      TODO: Test your code
-                      TODO: Learn
-                      """,
-                    spec -> spec.path(Path.of("TODO.md")).noTrim()
-            )
-        );
-    }
-
-    @Disabled
-    @Test
-    void editExistingTodoFile() {
-        rewriteRun(
-            //language=java
-            java(
-            """
-          class A {
-              // TODO: Have fun
-              /* TODO: Test your code */
-              // Just a regular comment
-              public String foo() {
-                // TODO: Learn
-                return "bar";
-              }
-              // Another regular comment
-          }
-          """
-            ),
-            //language=markdown
-            text(
-                    "",
-                    """
-                      ## Test Header
-                      TODO: Have fun
-                      TODO: Test your code
-                      TODO: Learn
-                      """ + "\n",
-                    spec -> spec.path(Path.of("TODO.md")).noTrim()
-            )
-        );
-    }
-
-    @Disabled
-    @Test
-    void doNotTouchExistingCorrectFile() {
-        rewriteRun(
-            //language=java
-            java(
-                    """
-                  class A {
-                      // TODO: Have fun
-                      /* TODO: Test your code */
-                      // Just a regular comment
-                      public String foo() {
-                        // TODO: Learn
-                        return "bar";
-                      }
-                      // Another regular comment
+              class A {
+                  // TODO: Have fun
+                  /* TODO: Test your code */
+                  // Just a regular comment
+                  public String foo() {
+                    // TODO: Learn
+                    return "bar";
                   }
-                  """
-            ),
-            //language=markdown
-            text(
-                    """
-                    ## Test Header
-                    TODO: Have fun
-                    TODO: Test your code
-                    TODO: Learn
-                    """,
-                    spec -> spec.path(Path.of("TODO.md")).noTrim()
-            )
+                  // Another regular comment
+              }
+              """
+          )
         );
     }
 }
--- src/test/java/com/yourorg/TrackTodosTest.java
+++ src/test/java/com/yourorg/TrackTodosTest.java
@@ -1,5 +1,6 @@
 package com.yourorg;
 
+import com.yourorg.table.TodoCommentsReport;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
@@ -9,6 +10,7 @@ import org.openrewrite.test.SourceSpec;
 
 import java.nio.file.Path;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.openrewrite.java.Assertions.java;
 import static org.openrewrite.test.SourceSpecs.text;
 import static org.openrewrite.xml.Assertions.xml;
@@ -18,7 +20,7 @@ class TrackTodosTest implements RewriteTest {
     @Override
     public void defaults(RecipeSpec spec) {
         spec.recipe(new TrackTodos("## Test Header"))
-                .allSources(SourceSpec::noTrim);
+          .allSources(SourceSpec::noTrim);
     }
 
     @Disabled
@@ -28,40 +30,39 @@ class TrackTodosTest implements RewriteTest {
         // Notice how the before text is doesNotExist(), indicating that the file does not exist yet.
         // The after text is the content of the file after the recipe is applied.
         rewriteRun(
-                // TODO: Uncomment the lines below once you have implemented the recipe
-                //spec -> spec.dataTable(TodoCommentsReport.Row.class, rows -> {
-                //    assertThat(rows).containsExactly(
-                //            new TodoCommentsReport.Row("A.java", "TODO: Have fun", "class org.openrewrite.java.tree.J$MethodDeclaration"),
-                //            new TodoCommentsReport.Row("A.java", "TODO: Test your code", "class org.openrewrite.java.tree.J$MethodDeclaration"),
-                //            new TodoCommentsReport.Row("A.java", "TODO: Learn", "class org.openrewrite.java.tree.J$Return"));
-                //}),
-                //language=java
-                java(
-                """
-            class A {
-                // TODO: Have fun
-                /* TODO: Test your code */
-                // Just a regular comment
-                public String foo() {
-                  // TODO: Learn
-                  return "bar";
-                }
-                // Another regular comment
-            }
+          spec -> spec.dataTable(TodoCommentsReport.Row.class, rows -> {
+              assertThat(rows).containsExactly(
+                new TodoCommentsReport.Row("A.java", "TODO: Have fun", "class org.openrewrite.java.tree.J$MethodDeclaration"),
+                new TodoCommentsReport.Row("A.java", "TODO: Test your code", "class org.openrewrite.java.tree.J$MethodDeclaration"),
+                new TodoCommentsReport.Row("A.java", "TODO: Learn", "class org.openrewrite.java.tree.J$Return"));
+          }),
+          //language=java
+          java(
             """
-                ),
-                //language=markdown
-                text(
-                        doesNotExist(),
-                        """
-                          ## Test Header
-                          TODO: Have fun
-                          TODO: Test your code
-                          TODO: Learn
-                          """,
-                        spec -> spec.path(Path.of("TODO.md")
-                        )
-                )
+              class A {
+                  // TODO: Have fun
+                  /* TODO: Test your code */
+                  // Just a regular comment
+                  public String foo() {
+                    // TODO: Learn
+                    return "bar";
+                  }
+                  // Another regular comment
+              }
+              """
+          ),
+          //language=markdown
+          text(
+            doesNotExist(),
+            """
+              ## Test Header
+              TODO: Have fun
+              TODO: Test your code
+              TODO: Learn
+              """,
+            spec -> spec.path(Path.of("TODO.md")
+            )
+          )
         );
     }
 
@@ -69,40 +70,39 @@ class TrackTodosTest implements RewriteTest {
     @Test
     void editExistingTodoFileJava() {
         rewriteRun(
-                // TODO: Uncomment the lines below once you have implemented the recipe
-                //spec -> spec.dataTable(TodoCommentsReport.Row.class, rows -> {
-                //    assertThat(rows).containsExactly(
-                //            new TodoCommentsReport.Row("A.java", "TODO: Have fun", "class org.openrewrite.java.tree.J$MethodDeclaration"),
-                //            new TodoCommentsReport.Row("A.java", "TODO: Test your code", "class org.openrewrite.java.tree.J$MethodDeclaration"),
-                //            new TodoCommentsReport.Row("A.java", "TODO: Learn", "class org.openrewrite.java.tree.J$Return"));
-                //}),
-                //language=java
-                java(
-                """
-            class A {
-                // TODO: Have fun
-                /* TODO: Test your code */
-                // Just a regular comment
-                public String foo() {
-                  // TODO: Learn
-                  return "bar";
-                }
-                // Another regular comment
-            }
+          spec -> spec.dataTable(TodoCommentsReport.Row.class, rows -> {
+              assertThat(rows).containsExactly(
+                new TodoCommentsReport.Row("A.java", "TODO: Have fun", "class org.openrewrite.java.tree.J$MethodDeclaration"),
+                new TodoCommentsReport.Row("A.java", "TODO: Test your code", "class org.openrewrite.java.tree.J$MethodDeclaration"),
+                new TodoCommentsReport.Row("A.java", "TODO: Learn", "class org.openrewrite.java.tree.J$Return"));
+          }),
+          //language=java
+          java(
             """
-                ),
-                //language=markdown
-                text(
-                        "",
-                        """
-                          ## Test Header
-                          TODO: Have fun
-                          TODO: Test your code
-                          TODO: Learn
-                          """ + "\n",
-                        spec -> spec.path(Path.of("TODO.md")
-                        )
-                )
+              class A {
+                  // TODO: Have fun
+                  /* TODO: Test your code */
+                  // Just a regular comment
+                  public String foo() {
+                    // TODO: Learn
+                    return "bar";
+                  }
+                  // Another regular comment
+              }
+              """
+          ),
+          //language=markdown
+          text(
+            "",
+            """
+              ## Test Header
+              TODO: Have fun
+              TODO: Test your code
+              TODO: Learn
+              """ + "\n",
+            spec -> spec.path(Path.of("TODO.md")
+            )
+          )
         );
     }
 
@@ -112,38 +112,37 @@ class TrackTodosTest implements RewriteTest {
         // Notice how the before text is doesNotExist(), indicating that the file does not exist yet.
         // The after text is the content of the file after the recipe is applied.
         rewriteRun(
-                // TODO: Uncomment the lines below once you have implemented the recipe
-                //spec -> spec.dataTable(TodoCommentsReport.Row.class, rows -> {
-                //    assertThat(rows).containsExactly(
-                //            new TodoCommentsReport.Row("file.yaml", "TODO: Have fun", "class org.openrewrite.yaml.tree.Yaml$Document"),
-                //            new TodoCommentsReport.Row("file.yaml", "TODO: Test your code", "class org.openrewrite.yaml.tree.Yaml$Mapping$Entry"),
-                //            new TodoCommentsReport.Row("file.yaml", "TODO: Learn", "class org.openrewrite.yaml.tree.Yaml$Mapping$Entry"));
-                //}),
-                //language=yaml
-                yaml(
-                """
-            # TODO: Have fun
-            someyaml: "here"
-            moreyaml: "there"
-            # TODO: Test your code
-            # TODO: Learn
-            # Just a regular comment
-            tabs:
-              are: "fun"
+          spec -> spec.dataTable(TodoCommentsReport.Row.class, rows -> {
+              assertThat(rows).containsExactly(
+                new TodoCommentsReport.Row("file.yaml", "TODO: Have fun", "class org.openrewrite.yaml.tree.Yaml$Document"),
+                new TodoCommentsReport.Row("file.yaml", "TODO: Test your code", "class org.openrewrite.yaml.tree.Yaml$Mapping$Entry"),
+                new TodoCommentsReport.Row("file.yaml", "TODO: Learn", "class org.openrewrite.yaml.tree.Yaml$Mapping$Entry"));
+          }),
+          //language=yaml
+          yaml(
             """
-                ),
-                //language=markdown
-                text(
-                        doesNotExist(),
-                        """
-                          ## Test Header
-                          TODO: Have fun
-                          TODO: Test your code
-                          TODO: Learn
-                          """,
-                        spec -> spec.path(Path.of("TODO.md")
-                        )
-                )
+              # TODO: Have fun
+              someyaml: "here"
+              moreyaml: "there"
+              # TODO: Test your code
+              # TODO: Learn
+              # Just a regular comment
+              tabs:
+                are: "fun"
+              """
+          ),
+          //language=markdown
+          text(
+            doesNotExist(),
+            """
+              ## Test Header
+              TODO: Have fun
+              TODO: Test your code
+              TODO: Learn
+              """,
+            spec -> spec.path(Path.of("TODO.md")
+            )
+          )
         );
     }
 
@@ -151,38 +150,37 @@ class TrackTodosTest implements RewriteTest {
     @Test
     void editExistingTodoFileYAML() {
         rewriteRun(
-                // TODO: Uncomment the lines below once you have implemented the recipe
-                //spec -> spec.dataTable(TodoCommentsReport.Row.class, rows -> {
-                //    assertThat(rows).containsExactly(
-                //            new TodoCommentsReport.Row("file.yaml", "TODO: Have fun", "class org.openrewrite.yaml.tree.Yaml$Document"),
-                //            new TodoCommentsReport.Row("file.yaml", "TODO: Test your code", "class org.openrewrite.yaml.tree.Yaml$Mapping$Entry"),
-                //            new TodoCommentsReport.Row("file.yaml", "TODO: Learn", "class org.openrewrite.yaml.tree.Yaml$Mapping$Entry"));
-                //}),
-                //language=yaml
-                yaml(
-                """
-            # TODO: Have fun
-            someyaml: "here"
-            moreyaml: "there"
-            # TODO: Test your code
-            # TODO: Learn
-            # Just a regular comment
-            tabs:
-              are: "fun"
+          spec -> spec.dataTable(TodoCommentsReport.Row.class, rows -> {
+              assertThat(rows).containsExactly(
+                new TodoCommentsReport.Row("file.yaml", "TODO: Have fun", "class org.openrewrite.yaml.tree.Yaml$Document"),
+                new TodoCommentsReport.Row("file.yaml", "TODO: Test your code", "class org.openrewrite.yaml.tree.Yaml$Mapping$Entry"),
+                new TodoCommentsReport.Row("file.yaml", "TODO: Learn", "class org.openrewrite.yaml.tree.Yaml$Mapping$Entry"));
+          }),
+          //language=yaml
+          yaml(
             """
-                ),
-                //language=markdown
-                text(
-                        "",
-                        """
-                          ## Test Header
-                          TODO: Have fun
-                          TODO: Test your code
-                          TODO: Learn
-                          """ + "\n",
-                        spec -> spec.path(Path.of("TODO.md")
-                        )
-                )
+              # TODO: Have fun
+              someyaml: "here"
+              moreyaml: "there"
+              # TODO: Test your code
+              # TODO: Learn
+              # Just a regular comment
+              tabs:
+                are: "fun"
+              """
+          ),
+          //language=markdown
+          text(
+            "",
+            """
+              ## Test Header
+              TODO: Have fun
+              TODO: Test your code
+              TODO: Learn
+              """ + "\n",
+            spec -> spec.path(Path.of("TODO.md")
+            )
+          )
         );
     }
 
@@ -192,38 +190,37 @@ class TrackTodosTest implements RewriteTest {
         // Notice how the before text is doesNotExist(), indicating that the file does not exist yet.
         // The after text is the content of the file after the recipe is applied.
         rewriteRun(
-                // TODO: Uncomment the lines below once you have implemented the recipe
-                //spec -> spec.dataTable(TodoCommentsReport.Row.class, rows -> {
-                //    assertThat(rows).containsExactly(
-                //            new TodoCommentsReport.Row("file.xml", "TODO: Have fun", "class org.openrewrite.xml.tree.Xml$Prolog"),
-                //            new TodoCommentsReport.Row("file.xml", "TODO: Test your code", "class org.openrewrite.xml.tree.Xml$Prolog"),
-                //            new TodoCommentsReport.Row("file.xml", "TODO: Learn", "class org.openrewrite.xml.tree.Xml$Tag"));
-                //}),
-                //language=xml
-                xml(
-                """
-            <!-- TODO: Have fun -->
-            <!-- TODO: Test your code -->
-            <xml>
-                <is>too</is>
-                <!-- TODO: Learn -->
-                <verbose>for me</verbose>
-            <!-- Just a regular comment -->
-            </xml>
+          spec -> spec.dataTable(TodoCommentsReport.Row.class, rows -> {
+              assertThat(rows).containsExactly(
+                new TodoCommentsReport.Row("file.xml", "TODO: Have fun", "class org.openrewrite.xml.tree.Xml$Prolog"),
+                new TodoCommentsReport.Row("file.xml", "TODO: Test your code", "class org.openrewrite.xml.tree.Xml$Prolog"),
+                new TodoCommentsReport.Row("file.xml", "TODO: Learn", "class org.openrewrite.xml.tree.Xml$Tag"));
+          }),
+          //language=xml
+          xml(
             """
-                ),
-                //language=markdown
-                text(
-                        doesNotExist(),
-                        """
-                          ## Test Header
-                          TODO: Have fun
-                          TODO: Test your code
-                          TODO: Learn
-                          """,
-                        spec -> spec.path(Path.of("TODO.md")
-                        )
-                )
+              <!-- TODO: Have fun -->
+              <!-- TODO: Test your code -->
+              <xml>
+                  <is>too</is>
+                  <!-- TODO: Learn -->
+                  <verbose>for me</verbose>
+              <!-- Just a regular comment -->
+              </xml>
+              """
+          ),
+          //language=markdown
+          text(
+            doesNotExist(),
+            """
+              ## Test Header
+              TODO: Have fun
+              TODO: Test your code
+              TODO: Learn
+              """,
+            spec -> spec.path(Path.of("TODO.md")
+            )
+          )
         );
     }
 
@@ -231,38 +228,37 @@ class TrackTodosTest implements RewriteTest {
     @Test
     void editExistingTodoFileXML() {
         rewriteRun(
-                // TODO: Uncomment the lines below once you have implemented the recipe
-                //spec -> spec.dataTable(TodoCommentsReport.Row.class, rows -> {
-                //    assertThat(rows).containsExactly(
-                //            new TodoCommentsReport.Row("file.xml", "TODO: Have fun", "class org.openrewrite.xml.tree.Xml$Prolog"),
-                //            new TodoCommentsReport.Row("file.xml", "TODO: Test your code", "class org.openrewrite.xml.tree.Xml$Prolog"),
-                //            new TodoCommentsReport.Row("file.xml", "TODO: Learn", "class org.openrewrite.xml.tree.Xml$Tag"));
-                //}),
-                //language=xml
-                xml(
-                """
-            <!-- TODO: Have fun -->
-            <!-- TODO: Test your code -->
-            <xml>
-                <is>too</is>
-                <!-- TODO: Learn -->
-                <verbose>for me</verbose>
-            <!-- Just a regular comment -->
-            </xml>
+          spec -> spec.dataTable(TodoCommentsReport.Row.class, rows -> {
+              assertThat(rows).containsExactly(
+                new TodoCommentsReport.Row("file.xml", "TODO: Have fun", "class org.openrewrite.xml.tree.Xml$Prolog"),
+                new TodoCommentsReport.Row("file.xml", "TODO: Test your code", "class org.openrewrite.xml.tree.Xml$Prolog"),
+                new TodoCommentsReport.Row("file.xml", "TODO: Learn", "class org.openrewrite.xml.tree.Xml$Tag"));
+          }),
+          //language=xml
+          xml(
             """
-                ),
-                //language=markdown
-                text(
-                        "",
-                        """
-                          ## Test Header
-                          TODO: Have fun
-                          TODO: Test your code
-                          TODO: Learn
-                          """ + "\n",
-                        spec -> spec.path(Path.of("TODO.md")
-                        )
-                )
+              <!-- TODO: Have fun -->
+              <!-- TODO: Test your code -->
+              <xml>
+                  <is>too</is>
+                  <!-- TODO: Learn -->
+                  <verbose>for me</verbose>
+              <!-- Just a regular comment -->
+              </xml>
+              """
+          ),
+          //language=markdown
+          text(
+            "",
+            """
+              ## Test Header
+              TODO: Have fun
+              TODO: Test your code
+              TODO: Learn
+              """ + "\n",
+            spec -> spec.path(Path.of("TODO.md")
+            )
+          )
         );
     }
 }
